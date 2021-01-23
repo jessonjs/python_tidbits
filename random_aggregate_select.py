d17f492f1f01c127a293954040e747886fbcac10
Jess Brisson <jessbrisson@gmail.com>
2021-01-23 09:21:07 -0600

    rename file and function to match what it does better

--- random_aggregate_select.py
+++ random_aggregate_select.py
@@ -1,6 +1,6 @@
 import random
 
-def randomBinaryItemSelect(iterations, items):
+def randomAggregateItemSelect(iterations, items):
     itemsAmount = {}
 
     for _ in range(iterations):
@@ -29,10 +29,10 @@ def getItemsSelectedPercentages(itemsSelected):
 
 coin = ["heads", "tails"]
 
-coinFlipped10 = randomBinaryItemSelect(10, coin)
-coinFlipped100 = randomBinaryItemSelect(100, coin)
-coinFlipped1000 = randomBinaryItemSelect(1000, coin)
-coinFlipped1000000 = randomBinaryItemSelect(1000000, coin)
+coinFlipped10 = randomAggregateItemSelect(10, coin)
+coinFlipped100 = randomAggregateItemSelect(100, coin)
+coinFlipped1000 = randomAggregateItemSelect(1000, coin)
+coinFlipped1000000 = randomAggregateItemSelect(1000000, coin)
 
 print("Coin flipped 10 times resulted in:", getItemsSelectedPercentages(coinFlipped10))
 print("Coin flipped 100 times resulted in:", getItemsSelectedPercentages(coinFlipped100))
@@ -42,10 +42,10 @@ print("Coin flipped 1000000 times resulted in:", getItemsSelectedPercentages(coi
 
 marbles = ["red", "red", "red", "red", "white", "red", "red", "red", "red", "red"]
 
-marbleGrabbed10 = randomBinaryItemSelect(10, marbles)
-marbleGrabbed100 = randomBinaryItemSelect(100, marbles)
-marbleGrabbed1000 = randomBinaryItemSelect(1000, marbles)
-marbleGrabbed1000000 = randomBinaryItemSelect(1000000, marbles)
+marbleGrabbed10 = randomAggregateItemSelect(10, marbles)
+marbleGrabbed100 = randomAggregateItemSelect(100, marbles)
+marbleGrabbed1000 = randomAggregateItemSelect(1000, marbles)
+marbleGrabbed1000000 = randomAggregateItemSelect(1000000, marbles)
 
 print("Marble grabbed 10 times resulted in:", getItemsSelectedPercentages(marbleGrabbed10))
 print("Marble grabbed 100 times resulted in:", getItemsSelectedPercentages(marbleGrabbed100))
